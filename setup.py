313b3cc2963991c65e5f0a262a6fb025ccf3c49c
Anthony <ab@outofpluto.com>
2015-09-03 17:26:04 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='starmato-pdf',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=['reportlab>=3.1',],
     package_data={
